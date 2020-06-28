049fd161074282fa00ed82158410f9632135b6c6
Ubuntu <pc@pcvm1.nak2sjuxe3medf0twl5niuwd4g.ix.internal.cloudapp.net>
2020-06-28 00:47:34 +0000

    Changes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM nginx:1.19.0-alpine
-COPY . /nginx-startup
-WORKDIR /nginx-startup
+RUN mkdir /app
+COPY startup.sh /app
+WORKDIR /app
 RUN apk add wget
 RUN apk add jq
 EXPOSE 80 443
--- update-docker-image.sh
+++ update-docker-image.sh
@@ -1,10 +1,13 @@
+docker_hub='pchahal24'
+repo='k8s-nginx'
 tag=$1
 if [ -z $tag ]
 then
   tag='latest'
 fi
-sudo docker pull pchahal24/k8s-nginx:$tag
-sudo docker ps | grep k8s-nginx | awk '{print $1}' | xargs sudo docker stop
-sudo docker rm k8s-nginx
-sudo docker run -d -p 80:80 -p 443:443 --restart=always --name k8s-nginx  pchahal24/k8s-nginx:$tag
+image_url='$docker_hub/$repo'
+sudo docker pull $image_url:$tag
+sudo docker ps | grep $repo | awk '{print $1}' | xargs sudo docker stop
+sudo docker rm $repo
+sudo docker run -d -p 80:80 -p 443:443 --restart=always --name $repo  $image_url:$tag
 
