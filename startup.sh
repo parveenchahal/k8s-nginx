ca1e377289a5edb404591630adc7265f77da13c3
Parveen Chahal <mail.chahal@gmail.com>
2021-02-27 00:34:15 +0000

    changed k8s service and startup script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,12 @@
+extract_pfx() {
+  name=$1
+  path=$2
+  openssl pkcs12 -clcerts -nokeys -in $path -out "$name.crt" -password pass: -passin pass:
+  openssl pkcs12 -nocerts -in $path -out "$name.key" -password pass: -passin pass: -passout pass:abcxyz
+  openssl rsa -in "$name.key" -out "$name.key" -passin pass:abcxyz
+}
+
+
 echo "Downloading nginx.conf..."
 wget -O /etc/nginx/nginx.conf "https://pchahal.blob.core.windows.net/nginx/nginx.conf"
 wget -O /etc/nginx/conf.d/richtable.conf "https://pchahal.blob.core.windows.net/nginx/richtable.conf"
@@ -6,23 +15,18 @@ wget -O /etc/nginx/conf.d/amitchahal.conf "https://pchahal.blob.core.windows.net
 wget -O /etc/nginx/conf.d/parveenchahal.conf "https://pchahal.blob.core.windows.net/nginx/parveenchahal.conf"
 
 echo "Fetching access token for keyvault..."
-identity_url="http://aad-identity-service.default:2424/$AAD_IDENTITY_TENANT?client_id=$AAD_IDENTITY_CLIENTID&secret=$AAD_IDENTITY_SECRET"
+identity_url="https://authonline.net/aadtoken/$AAD_IDENTITY_TENANT?client_id=$AAD_IDENTITY_CLIENTID&secret=$AAD_IDENTITY_SECRET"
 identity_url="$identity_url&resource=https://vault.azure.net"
-accesstoken=$(curl -sS $identity_url | jq -r '.access_token')
+access_token=$(curl -sS $identity_url | jq -r '.access_token')
 
-curl -sS "https://pckv1.vault.azure.net/secrets/key-pass?api-version=2016-10-01" -H "Authorization: Bearer $accesstoken" | jq -r '.value' | base64 -d > /etc/ssl/key-pass
-
-secrets="richtable-in parveenchahal-com authonline-net"
+secrets="richtable-in parveenchahal-com authonline-net pcapis-com"
 for i in $secrets; do
   echo "Downloading secret $i..."
-  x=$(curl -sS "https://pckv1.vault.azure.net/secrets/$i?api-version=2016-10-01" -H "Authorization: Bearer $accesstoken" | jq -r '.value')
-  crt="$(echo $x | cut -d'|' -f1)"
-  name="$i.crt"
-  echo $crt | base64 -d > /etc/ssl/$name
-
-  key="$(echo $x | cut -d'|' -f2)"
-  name="$i.key"
-  echo $key | base64 -d > /etc/ssl/$name
+  pfx="$i.pfx"
+  curl -sS "https://pckv1.vault.azure.net/secrets/$i?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value' | base64 -d > $pfx
+  extract_pfx "$i" $pfx
+  mv "$i.crt" "$i.key" > /etc/ssl/
+  rm $pfx
 done
 
 nginx
